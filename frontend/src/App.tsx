3233e90a9a6368ba5d66583693353393cc104a6f
Joseildo <joseildo.sfc@gmail.com>
2022-09-05 22:24:06 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
 
   return (
-   <h1>Olá Mundo</h1>
+
+    <>
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+    </>
+    
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,15 @@
+import Vector from '../../assets/img/Vector.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+
+    return (
+        
+        <div className="dsmeta-red-btn">
+          <img src={Vector} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
